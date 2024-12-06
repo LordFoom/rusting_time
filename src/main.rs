9adabb98e2e8039253406d551e012d6678324ddb
Timothy Spring <foom@amazon.com>
2024-12-06 15:26:25 +0200

    Timer arg is now minutes
    Was seconds before with 30 min default.
    Minutes is more convenient it turns out.

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,74 @@
+use clap::{arg, Parser};
+use crossterm::style::Stylize;
+use crossterm::terminal::size;
+use crossterm::{cursor, style, terminal, ExecutableCommand, QueueableCommand};
 use std::error::Error;
 use std::io::{stdout, Write};
 use std::time::{Duration, Instant};
-use clap::{arg, Parser};
-use crossterm::{cursor, ExecutableCommand, QueueableCommand, style, terminal};
-use crossterm::style::Stylize;
-use crossterm::terminal::size;
 #[derive(Parser)]
 #[command(name = "")]
 #[command(author = "Foom")]
-#[command(version = "0.1")]
+#[command(version = "1.0")]
 #[command(
-about = "The passage of time is the rust of clocks",
-long_about = "none"
+    about = "The passage of time is the rust of clocks",
+    long_about = "none"
 )]
-struct Args{
+struct Args {
     #[arg(long, short)]
-    time: Option<String>,
+    time: Option<u128>,
 }
 
-struct Timer{
+struct Timer {
     current_duration: Duration,
-
 }
 ///Count down timer for the terminal
-fn main()->Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    //how many seconds are we going to do?
-    let mut millis = if let Some(t) = args.time{
-        t.parse::<u128>()? * 1000
+    //how many minutes are we going to do?
+    let millis = if let Some(t) = args.time {
+        t * 1000 * 60
     } else {
         //half an hour
-        30*60*1000
+        30 * 60 * 1000
         //but for testing, 30s
         // 30*1000
     };
     //get the original size
-    let (cols,rows) = size()?;
+    let (cols, rows) = size()?;
     let mut stdout = stdout();
     stdout.execute(terminal::Clear(terminal::ClearType::All))?;
-    stdout.execute(terminal::SetSize(4,12))?;
+    stdout.execute(terminal::SetSize(4, 12))?;
     stdout.execute(terminal::SetTitle("Time is the Rust of Clocks"))?;
     //set the start time
 
-    // let mut terminal =
-    let start_instant = Instant::now();
-    //set the label
-    let pause_msieur = Duration::from_millis(100);
-    loop{
-        print!("\x1B[2J");
-        // println!("I like to parse it parse it {}", millis);
-        let elapsed_millis = start_instant.elapsed().as_millis();
-        // println!("elapsed={}, millis={}", elapsed_millis, millis);
-        let remaining_millis = if elapsed_millis <  millis {
-            millis- elapsed_millis
-        }else{
-            0
-        };
-        stdout.queue(cursor::MoveTo(5,5))?;
-        stdout.queue(style::PrintStyledContent(format_millis(remaining_millis).cyan()))?;
-        stdout.flush()?;
-        std::thread::sleep(pause_msieur);
-        if remaining_millis <= 0{
+    let mut count = 0;
+    loop {
+        let start_instant = Instant::now();
+        //set the label
+        let pause_msieur = Duration::from_millis(100);
+        loop {
+            //clear the screen, don't want litter
+            print!("\x1B[2J");
+            let elapsed_millis = start_instant.elapsed().as_millis();
+            let remaining_millis = if elapsed_millis < millis {
+                millis - elapsed_millis
+            } else {
+                0
+            };
+            stdout.queue(cursor::MoveTo(5, 5))?;
+            stdout.queue(style::PrintStyledContent(
+                format_millis(remaining_millis).cyan(),
+            ))?;
+            stdout.queue(style::PrintStyledContent(format_count(count).yellow()))?;
+            stdout.flush()?;
+            std::thread::sleep(pause_msieur);
+            if remaining_millis <= 0 {
+                break;
+            }
+        }
+
+        count += 1;
+        if count > 10 {
             break;
         }
     }
@@ -68,13 +76,18 @@ fn main()->Result<(), Box<dyn Error>> {
     stdout.execute(terminal::SetSize(cols, rows))?;
     // println!("That's the spiritus!");
     Ok(())
-    //start the timer
 }
 
-fn format_millis(millis: u128)->String {
+fn format_millis(millis: u128) -> String {
     let seconds = millis / 1000;
     let minutes = seconds / 60;
     let remaining_seconds = seconds % 60;
     let remaining_millis = millis % 1000;
-    format!("{:0>3}:{:0>2}.{}", minutes, remaining_seconds, remaining_millis)
+    format!(
+        "{:0>3}:{:0>2}.{} ",
+        minutes, remaining_seconds, remaining_millis
+    )
+}
+fn format_count(count: u128) -> String {
+    format!("count={}", count)
 }
